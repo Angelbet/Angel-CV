2036b05d1552d5d434ec9fe17d227b0d6ba6e129
Angelbet <angelfabian.becerra@gmail.com>
2022-05-16 21:42:47 -0400

    Multilenguaje

--- next.config.js
+++ next.config.js
@@ -5,9 +5,11 @@
 
 // module.exports = nextConfig
 
-module.exports = {
-  i18n: {
-    locales: ['es', 'en'],
-    defaultLocale: 'es',
-  },
+const { i18n } = require("./next-i18next.config");
+
+const nextConfig = {
+  // other stuff
+  i18n,
 };
+
+module.exports = nextConfig;
